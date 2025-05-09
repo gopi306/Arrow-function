6b474af4f4258724dde9a3e7f1f4e9718427d552
Gopi Nath <gopinathreddy4362@gmail.com>
2025-05-09 21:19:04 +0530

    version

---
+++ app.js
@@ -0,0 +1,15 @@
+// output is coming an array
+
+let Todo= []
+
+function Add(){
+    let inputValue =document.querySelector('#input');
+
+    let name =inputValue.value;
+    // how to get output is array values .
+   Todo.push(name);
+   console.log(Todo);
+   inputValue.value='';
+   
+}
+
